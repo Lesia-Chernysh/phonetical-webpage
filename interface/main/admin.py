d43f1fe8fd2d16ffd2e7ef2d78c80c2fa462c3f1
Lesia-Chernysh <91280391+Lesia-Chernysh@users.noreply.github.com>
2022-08-01 00:39:12 +0200

    Delete interface/main directory

--- interface/main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import InformationAboutVowels
-from .models import InformationAboutConsonants
-
-admin.site.register(InformationAboutVowels)
-admin.site.register(InformationAboutConsonants)
